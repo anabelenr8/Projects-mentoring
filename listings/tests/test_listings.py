584c7540c14aab85e729ff15f89725b39d7236ed
Ana Belen Romero <ana.romero0501@gmail.com>
2024-01-20 15:42:41 +0000

    First Draft

--- listings/tests/test_listings.py
+++ listings/tests/test_listings.py
@@ -1,6 +1,5 @@
 import unittest
-from typing import Dict, Any
-from unittest.mock import MagicMock, patch
+from unittest.mock import patch
 
 from listings.base import ListingsRequest
 
@@ -8,46 +7,43 @@ listings_request = ListingsRequest(
     api_key='736c751d-0ecd-426a-80cf-4a9ab782360e',
     api_token='nBKd0EeSeNwtqau3KSzKMqUPsO7MgcLKMkP' \
               'IGmJHX3NzrvXmCxkkdqysWO5WZXwozKf61b65_bQsXomIiSIi9g',
-    hostname='https://api.pythonic.me/v1'
-)
+    hostname='https://api.pythonic.me/v1')
 
 
 class TestListingsRequest(unittest.TestCase):
-    def mock_response_method(self, data: Dict[str, Any]):
-        mock_response = MagicMock()
-        mock_response.status_code = 200
-        mock_response.json.return_value = data
-        return mock_response
-
     @patch('listings.base.ListingsRequest.get_digital_listings')
     def test_get_digital_listings(self, mock_get_listings):
-        mock_res = self.mock_response_method(
-            {'listing_type': 'digital'}
-        )
-        mock_get_listings.return_value = mock_res
+        expected_result = [{'listing_type': 'digital'}]
+
+        mock_get_listings.return_value = expected_result
 
         request = listings_request
 
         result = request.get_digital_listings()
 
-        self.assertEqual(result, mock_res)
+        self.assertEqual(result, expected_result)
 
     @patch('listings.base.ListingsRequest.get_soft_tag_listings')
     def test_get_soft_tag_listings(self, mock_get_listings):
-        mock_res = self.mock_response_method(
-            {'tag': 'soft'}
-        )
-        mock_get_listings.return_value = mock_res
+        expected_result = [{'tag': 'soft'}]
+
+        mock_get_listings.return_value = expected_result
 
         request = listings_request
 
         result = request.get_soft_tag_listings()
 
-        self.assertEqual(result, mock_res)
+        self.assertEqual(result, expected_result)
 
     @patch('listings.base.ListingsRequest.get_usd_listings_under_20')
     def test_get_usd_listings_under_20(self, mock_get_listings):
-        params = [
+        expected_data_contains_listings_under_20 = [
+            {'currency_code': 'usd', 'price_amount': 10},
+            {'currency_code': 'usd', 'price_amount': 15},
+            {'currency_code': 'usd', 'price_amount': 20},
+        ]
+
+        expected_data_does_not_contains_listings_under_20 = [
             {'currency_code': 'usd', 'price_amount': 10},
             {'currency_code': 'usd', 'price_amount': 15},
             {'currency_code': 'usd', 'price_amount': 20},
@@ -55,60 +51,78 @@ class TestListingsRequest(unittest.TestCase):
             {'currency_code': 'usd', 'price_amount': 30}
         ]
 
-        mock_res = [self.mock_response_method(data=param) for param in params
-                    if param.get('currency_code') == 'usd'
-                    and int(param['price_amount']) <= 20]
-
-        mock_get_listings.return_value = mock_res
+        mock_get_listings.side_effect = \
+            [expected_data_contains_listings_under_20,
+             expected_data_does_not_contains_listings_under_20]
 
         request = listings_request
 
         result = request.get_usd_listings_under_20()
 
-        expected_result = [
-            {'currency_code': 'usd', 'price_amount': 10},
-            {'currency_code': 'usd', 'price_amount': 15},
-            {'currency_code': 'usd', 'price_amount': 20}
-        ]
+        expected_result = [item for item in
+                           expected_data_contains_listings_under_20 if
+                           item['price_amount'] <= 20]
 
-        self.assertEqual(result, mock_res, expected_result)
+        self.assertEqual(result, expected_result)
 
     @patch('listings.base.ListingsRequest.get_blanket_search_listings')
     def test_get_blanket_search_listings(self, mock_get_listings):
-        mock_data = [
-            {
-                'title': 'Soft Wool Blanket',
-                'description': 'Warm and cozy blanket',
-            },
-            {
-                'title': 'Cotton Throw Pillow',
-                'description': 'Decorative pillow',
-            },
-            {
-                'title': 'Wool Socks',
-                'description': 'Comfortable woolen socks',
-            },
+        expected_result_contains_blanket = [
+            {'title': 'Soft Wool Blanket',
+             'description': 'Warm and cozy blanket', }
         ]
-        filtered_data = [entry for entry in mock_data if
-                         'blanket' in entry['title'] or
-                         'blanket' in entry['description']]
 
-        mock_res = [self.mock_response_method(data=entry)
-                    for entry in filtered_data]
+        expected_result_does_not_contain_blanket = [
+            {'title': 'Cotton Throw Pillow',
+             'description': 'Decorative pillow', }
+        ]
 
-        mock_get_listings.return_value = mock_res
+        mock_get_listings.side_effect = \
+            [expected_result_contains_blanket,
+             expected_result_does_not_contain_blanket]
 
         request = listings_request
 
-        result = request.get_blanket_search_listings()
+        result_contains_blanket = request.get_blanket_search_listings()
+        self.assertEqual(result_contains_blanket,
+                         expected_result_contains_blanket)
 
-        expected_result = [
-            {
-                'title': 'Soft Wool Blanket',
-                'description': 'Warm and cozy blanket',
-            }
+        result_does_not_contain_blanket = request.get_blanket_search_listings()
+        self.assertEqual(result_does_not_contain_blanket,
+                         expected_result_does_not_contain_blanket)
+
+    @patch('listings.base.ListingsRequest.get_cotton_eur_listings')
+    def test_get_cotton_eur_listings(self, mock_get_listings):
+        expected_result_meets_criteria = [
+            {'title': 'Soft Wool Blanket',
+             'description': 'Warm and cozy blanket',
+             'tag': 'cotton',
+             'currency_code': 'eur'}
         ]
-        self.assertEqual(result, expected_result)
+
+        expected_result_does_not_meet_criteria = [
+            {'title': 'Cotton Throw Pillow',
+             'description': 'Decorative pillow',
+             'tag': 'cotton',
+             'currency_code': 'usd'},
+            {'title': 'Wool Socks',
+             'description': 'Comfortable woolen socks',
+             'tag': 'wool',
+             'currency_code': 'eur'}
+        ]
+
+        mock_get_listings.side_effect = \
+            [expected_result_meets_criteria,
+             expected_result_does_not_meet_criteria]
+
+        request = listings_request
+
+        result_meets_criteria = request.get_cotton_eur_listings()
+        self.assertEqual(result_meets_criteria, expected_result_meets_criteria)
+
+        result_does_not_meet_criteria = request.get_cotton_eur_listings()
+        self.assertEqual(result_does_not_meet_criteria,
+                         expected_result_does_not_meet_criteria)
 
 
 if __name__ == '__main__':
