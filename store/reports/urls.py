bb8c576fa7c4ddc64f70217e957c83dd685028fe
Ana Belen Romero <ana.romero0501@gmail.com>
2024-03-13 15:03:45 +0000

    work to be reviewed

--- store/reports/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-
-from store.reports.views import get_api_survey_and_answers
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('/api/survey/', get_api_survey_and_answers)
-]
