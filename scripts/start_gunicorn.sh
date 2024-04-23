1029473cd933679765c585975d6450a96b248296
Ana Belen Romero <ana.romero0501@gmail.com>
2024-04-23 11:19:54 +0100

    start gunicorn script

---
+++ scripts/start_gunicorn.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+VENV_PATH="/var/www/.local/share/virtualenvs/store-LEsRl0nh"
+
+CURRENT_PROJECT_PATH="/var/www/projects/store"
+
+APP_PATH="${CURRENT_PROJECT_PATH}"
+
+APP="store"
+
+cd ${APP_PATH}
+
+exec ${VENV_PATH}/bin/gunicorn --workers 3 --bind unix:${APP_PATH}/${APP}.sock ${APP}.wsgi:application --chdir=${APP_PATH}
--- store/wsgi.py
+++ store/wsgi.py
@@ -10,6 +10,9 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv(filename='store.env', raise_error_if_not_found=True))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store.settings')
 
