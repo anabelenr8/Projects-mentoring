88698914a6468f4e679760cce1a6bb51b6c8ce50
Ana Belen Romero <ana.romero0501@gmail.com>
2024-08-05 23:33:30 +0100

    added swagger documentation

--- store/listings/views.py
+++ store/listings/views.py
@@ -1,43 +1,58 @@
+from typing import Dict
+
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import serializers, viewsets
 
 from .models import Listing
 
 
+def swagger_docs(methods: Dict):
+    def decorator(cls):
+        for method, kwargs in methods.items():
+            original_method = getattr(cls, method, None)
+            if original_method:
+                decorated_method = swagger_auto_schema(**kwargs)(
+                    original_method)
+                setattr(cls, method, decorated_method)
+        return cls
+
+    return decorator
+
+
 class ListingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Listing
         fields = [
-            'listing_id',
+            'id',
             'title',
             'price',
             'currency_code',
             'description',
             'tags',
-            'created_at',
-            'updated_at'
+            'created_on',
+            'updated_on'
         ]
 
 
+listing_methods = {
+    'list': {
+        'operation_summary': "List all listings",
+        'operation_description': "Retrieve a list"
+                                 " of all listings in the database",
+        'tags': ["Listings"],
+        'responses': {200: ListingSerializer(many=True)}
+    },
+    'retrieve': {
+        'operation_summary': "Retrieve a listing",
+        'operation_description': "Retrieve a specific listing by ID",
+        'tags': ["Listings"],
+        'responses': {200: ListingSerializer()}
+    }
+}
+
+
+@swagger_docs(listing_methods)
 class ListingViewSet(viewsets.ModelViewSet):
     queryset = Listing.objects.all()
     serializer_class = ListingSerializer
     lookup_field = 'uid'
-
-    @swagger_auto_schema(
-        operation_summary="List all listings",
-        operation_description="Retrieve a list of all listings in the database",
-        tags=["Listings"],
-        responses={200: ListingSerializer(many=True)}
-    )
-    def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
-
-    @swagger_auto_schema(
-        operation_summary="Retrieve a listing",
-        operation_description="Retrieve a specific listing by ID",
-        tags=["Listings"],
-        responses={200: ListingSerializer()}
-    )
-    def retrieve(self, request, *args, **kwargs):
-        return super().retrieve(request, *args, **kwargs)
