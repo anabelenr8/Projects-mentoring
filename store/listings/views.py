923cd9b97a04e222a621265bf3c9127117af2ef2
Ana Belen Romero <ana.romero0501@gmail.com>
2024-08-22 16:25:18 +0100

    added example for retriving a list in docs page

--- store/listings/views.py
+++ store/listings/views.py
@@ -1,7 +1,19 @@
+from drf_yasg import openapi
 from rest_framework import serializers, viewsets
 
 from .models import Listing
 from .utils import swagger_docs
+
+example_listing_response = {
+    "id": 1,
+    "title": "Baby Bottles - Probably",
+    "price": "1500",
+    "currency_code": "usd",
+    "description": "This is a reflect baby bottles. Tell a figure girl.",
+    "tags": ["soft", "wool"],
+    "created_on": "2023-08-22T10:00:00Z",
+    "updated_on": "2023-08-22T12:00:00Z"
+}
 
 
 class ListingSerializer(serializers.ModelSerializer):
@@ -22,16 +34,32 @@ class ListingSerializer(serializers.ModelSerializer):
 listing_methods = {
     'list': {
         'operation_summary': "List all listings",
-        'operation_description': "Retrieve a list"
-                                 " of all listings in the database",
+        'operation_description': "Retrieve a list of all "
+                                 "listings in the database",
         'tags': ["Listings"],
-        'responses': {200: ListingSerializer(many=True)}
+        'responses': {
+            200: openapi.Response(
+                description="A list of listings",
+                examples={
+                    "application/json": [example_listing_response]
+                },
+                schema=ListingSerializer(many=True)
+            )
+        }
     },
     'retrieve': {
         'operation_summary': "Retrieve a listing",
         'operation_description': "Retrieve a specific listing by ID",
         'tags': ["Listings"],
-        'responses': {200: ListingSerializer()}
+        'responses': {
+            200: openapi.Response(
+                description="A single listing",
+                examples={
+                    "application/json": example_listing_response
+                },
+                schema=ListingSerializer()
+            )
+        }
     },
     'create': {
         'auto_schema': None
